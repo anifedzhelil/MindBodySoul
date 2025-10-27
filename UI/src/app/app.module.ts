69679653e57f56c9e94b91b1307d687019755c3e
Anife Dzhelil <anifedzhelil@gmail.com>
2025-10-27 13:25:40 +0200

    Upgrade to Angular 18 - successful

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CloudinaryModule } from '@cloudinary/ng';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -15,32 +15,26 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ContactsComponent } from './contacts/contacts.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PageNotFoundComponent,
-    ContactsComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    UserModule,
-    CoreModule,
-    CloudinaryModule,
-    SharedModule,
-    AppRoutingModule,
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        PageNotFoundComponent,
+        ContactsComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FontAwesomeModule,
+        FormsModule,
+        ReactiveFormsModule,
+        UserModule,
+        CoreModule,
+        CloudinaryModule,
+        SharedModule,
+        AppRoutingModule], providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
