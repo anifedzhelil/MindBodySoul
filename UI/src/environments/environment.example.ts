0f5d3c25a68f824c7f68f733754b098171b22733
Anife Dzhelil <anifedzhelil@gmail.com>
2025-11-03 19:09:54 +0200

    Updated gitignore file

---
+++ UI/src/environments/environment.example.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:5020',
+  cloudinary: {
+    cloudName: 'YOUR_CLOUD_NAME',      
+    apiKey: 'YOUR_API_KEY',              
+    uploadPreset: 'YOUR_UPLOAD_PRESET',  
+  },
+};
--- UI/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-
-    apiBaseUrl: 'https://localhost:7197'
-};
