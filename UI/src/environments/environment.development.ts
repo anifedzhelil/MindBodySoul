c6ad419e05b8895b11a17c3db8834812de94f7af
Anife Dzhelil <anifedzhelil@gmail.com>
2025-11-03 19:09:23 +0200

    Update .gitignore and remove dev environment config

--- UI/src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'http://localhost:5020',
-  cloudinary: {
-    cloudName: 'drhqukmht',
-    apiKey: '999623514624233',
-    uploadPreset: 'mineBodySoulApp',
-  },
-};
