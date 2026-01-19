d8aae2c5b5ece7346a8f49e078d4b2b963c4f419
Anife Dzhelil <anifedzhelil@gmail.com>
2026-01-19 18:14:36 +0200

    Fix background image path

--- UI/src/app/home/home.component.ts
+++ UI/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
     selector: 'app-home',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
     standalone: false
 })
 export class HomeComponent {
-  backgroundImageUrl = '/assets/images/home6.jpg';
-
+ get backgroundStyle() {
+    return {
+      'background-image': `url(${environment.production ? '/MindBodySoul' : ''}/assets/images/home6.jpg)`
+    };
+  }
 }
