494312df7e54a4df4d92111bc150cc9ff592ab58
Anife Dzhelil <anifedzhelil@gmail.com>
2026-03-27 10:31:24 +0200

    Edit design and add latest-post component

---
+++ UI/src/app/shared/components/latest-posts/latest-posts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-posts',
+  imports: [],
+  templateUrl: './latest-posts.component.html',
+  styleUrl: './latest-posts.component.css'
+})
+export class LatestPostsComponent {
+
+}
