14d5836ad83824a98ea2ffce5e4749f16218c461
Anife Dzhelil <anifedzhelil@gmail.com>
2026-07-25 06:43:17 +0300

    featŞ add some controls

--- UI/src/app/admin/instagram/carousel/carousel.component.ts
+++ UI/src/app/admin/instagram/carousel/carousel.component.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   standalone: false
 })
 export class CarouselComponent {
+  isSizeOpen: boolean = true;
+  isTextOpen:  boolean = false;
 
+  paddingSide: number = 10;
+  paddingTop: number = 20;
+  description: string = '';
+  note: string = '';
+
+
+  onToggleSizeSection(): void{
+    this.isSizeOpen = !this.isSizeOpen;
+  }
+
+  onToggleTextSection(): void{
+    this.isTextOpen = !this.isTextOpen;
+
+  }
 }
